43bbf5238e16ce1be3bbfed6784ce8601455c4bb
eunsuneun <eunsunbsn@gmail.com>
2021-09-16 23:54:50 +0900

    webpack.config.prod.js 수정

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'production',
-  entry: './source/index.js',
+  entry: {
+    index: './source/index.js',
+    about: './source/about.js',
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'index-bundle.js',
+    filename: '[name]-bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './source/index.html',
+      filename: './index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './source/about.html',
+      filename: './about.html',
+      chunks: ['about'],
+    }),
+  ],
 };
